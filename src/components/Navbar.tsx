71d430364a9091008f52bca83467956def00a6b7
Ethan Tang <itsethang5@gmail.com>
2024-12-15 16:16:01 -0500

    Built a navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,18 @@
-import { Container, Text } from "@radix-ui/themes";
+import { Avatar, Box, Button, Container, Flex, Text } from "@radix-ui/themes";
+import logo from "../assets/pixel-space-icon.webp";
 
 const Navbar = () => {
   return (
     <nav className="border-b-2 mb-5 py-4 px-3">
       <Container>
-        <Text className="font-bold">Pixel Space</Text>
+        <Flex justify="between" align="center">
+          <Flex align="center">
+            <Avatar src={logo} fallback="PS" className="mr-2" />
+            <Text className="font-bold text-lg">Pixel Space</Text>
+          </Flex>
+
+          <Button>Login</Button>
+        </Flex>
       </Container>
     </nav>
   );
