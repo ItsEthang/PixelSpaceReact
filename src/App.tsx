c55fb6d6a6e9a6999ac69d661759efbd32e673a2
Ethan Tang <itsethang5@gmail.com>
2024-12-15 21:38:15 -0500

    Set up React query

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <Navbar />
       {/* <HomeGrid /> */}
       <Container>
-        {/* <UserRegistration /> */}
+        <UserRegistration />
         <UserLogin />
       </Container>
     </>
