97dfb62c8368acd88bcc72f32a84c1b926aa83f8
Ethan Tang <itsethang5@gmail.com>
2024-12-21 18:51:32 -0500

    Add line clamp to post cards

--- src/components/post/PostContent.tsx
+++ src/components/post/PostContent.tsx
@@ -1,14 +1,13 @@
 import { Box, Text } from "@radix-ui/themes";
-import React from "react";
 import { Post } from "../../interfaces/Entity";
 
 const PostContent = ({ post }: { post: Post }) => {
   return (
-    <Box>
+    <Box my="3">
       <Text as="div" size="6" weight="bold" mb="2">
         {post.title}
       </Text>
-      <Text as="div" size="2" color="gray">
+      <Text as="p" size="2" color="gray" className="line-clamp-3">
         {post.content}
       </Text>
     </Box>
