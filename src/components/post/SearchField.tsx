75cffe9d47d490463334cc3761df4dda1c206a4a
Ethan Tang <itsethang5@gmail.com>
2024-12-22 18:45:19 -0500

    Allow search field to be cleared

--- src/components/post/SearchField.tsx
+++ src/components/post/SearchField.tsx
@@ -1,26 +1,44 @@
-import { TextField } from "@radix-ui/themes";
+import { Button, Flex, IconButton, Text, TextField } from "@radix-ui/themes";
 import { useRef } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import usePostQueryStore from "./store";
+import { MdClear } from "react-icons/md";
 
 const SearchField = () => {
-  const { setTitle } = usePostQueryStore();
+  const { postQuery, setTitle } = usePostQueryStore();
   const ref = useRef<HTMLInputElement>(null);
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (ref.current) {
-          setTitle(ref.current.value);
-        }
-      }}
-    >
-      <TextField.Root placeholder="Search by title…" ref={ref}>
-        <TextField.Slot>
-          <FaMagnifyingGlass height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
-    </form>
+    <Flex align="center" gap="4">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (ref.current) {
+            setTitle(ref.current.value);
+          }
+        }}
+      >
+        <TextField.Root placeholder="Search by title…" ref={ref}>
+          <TextField.Slot>
+            <FaMagnifyingGlass height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
+      </form>
+      {postQuery.title && (
+        <>
+          <Text as="span" color="gray" size="1">
+            Title:{" " + postQuery.title}
+          </Text>
+          <Button
+            onClick={() => setTitle("")}
+            size="1"
+            variant="ghost"
+            color="red"
+          >
+            <MdClear />
+          </Button>
+        </>
+      )}
+    </Flex>
   );
 };
 
