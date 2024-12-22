6b0b3f401fc35aa10868de8f0cec1c63c3b9cfba
Ethan Tang <itsethang5@gmail.com>
2024-12-22 13:06:55 -0500

    Allow following  card to unfilter

--- src/components/follow/FollowingCard.tsx
+++ src/components/follow/FollowingCard.tsx
@@ -1,15 +1,25 @@
 import { Avatar, Card, Flex, Text } from "@radix-ui/themes";
 import { UserInfo } from "../../interfaces/Entity";
+import { useState } from "react";
 
 interface Props {
   following: UserInfo;
   handleClick: (userId: string | undefined) => void;
 }
 const FollowingCard = ({ following, handleClick }: Props) => {
+  const [isFiltered, setIsFiltered] = useState(false);
+  const onClick = () => {
+    if (isFiltered) {
+      handleClick(undefined);
+    } else {
+      handleClick(following.userId + "");
+    }
+    setIsFiltered(!isFiltered);
+  };
   return (
     <Card
       className="hover:bg-slate-800 transition-all w-40 xl:w-52 my-3"
-      onClick={() => handleClick(following.userId + "")}
+      onClick={onClick}
     >
       <Flex gap="3" align="center">
         <Avatar
