bc401c73901291206bd2af4b5692941c0dddffd4
Ethan Tang <itsethang5@gmail.com>
2024-12-22 16:38:25 -0500

    Implement store to post search

--- src/components/post/SearchField.tsx
+++ src/components/post/SearchField.tsx
@@ -1,13 +1,29 @@
 import { TextField } from "@radix-ui/themes";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import usePostQueryStore from "./store";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchField = () => {
+  const { setTitle } = usePostQueryStore();
+  const navigate = useNavigate();
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <TextField.Root placeholder="Search by title…">
-      <TextField.Slot>
-        <FaMagnifyingGlass height="16" width="16" />
-      </TextField.Slot>
-    </TextField.Root>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref.current) {
+          setTitle(ref.current.value);
+          navigate("/");
+        }
+      }}
+    >
+      <TextField.Root placeholder="Search by title…">
+        <TextField.Slot ref={ref}>
+          <FaMagnifyingGlass height="16" width="16" />
+        </TextField.Slot>
+      </TextField.Root>
+    </form>
   );
 };
 
--- src/hooks/useGetPost.ts
+++ src/hooks/useGetPost.ts
@@ -2,15 +2,29 @@ import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
 import { Post } from "../interfaces/Entity";
 import ms from "ms";
+import usePostQueryStore from "../components/post/store";
 
 const useGetPost = () => {
+  const postQuery = usePostQueryStore((s) => s.postQuery);
   const getPost = async () => {
-    const res = await apiClient.get<Post[]>("/post");
+    const params = new URLSearchParams();
+    if (postQuery.title) {
+      params.append("title", postQuery.title);
+    }
+    if (postQuery.username) {
+      params.append("username", postQuery.username);
+    }
+    const queryString = params.toString();
+
+    const res = await apiClient.get<Post[]>(
+      `/post${queryString ? "?" + queryString : ""}`
+    );
+
     const sortedPosts = res.data.sort((a, b) => b.timeCreated - a.timeCreated);
     return sortedPosts;
   };
   return useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    queryKey: ["posts", postQuery],
     queryFn: getPost,
     retry: 3,
     staleTime: ms("3m"),
