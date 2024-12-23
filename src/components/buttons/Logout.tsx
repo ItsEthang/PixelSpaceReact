2c8d42c22a65485c3834227182372495989caaef
Ethan Tang <itsethang5@gmail.com>
2024-12-23 15:40:50 -0500

    Implement useSignOut for logout

--- src/components/buttons/Logout.tsx
+++ src/components/buttons/Logout.tsx
@@ -1,11 +1,13 @@
 import { Button, Text } from "@radix-ui/themes";
+import useSignOut from "react-auth-kit/hooks/useSignOut";
 import { RiLogoutBoxLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
 const Logout = ({ logout }: { logout: () => void }) => {
+  const signOut = useSignOut();
   return (
     <Link to="/">
-      <Button onClick={logout} variant="surface">
+      <Button onClick={() => signOut()} variant="surface">
         <Text weight="bold">Logout</Text>
         <RiLogoutBoxLine />
       </Button>
