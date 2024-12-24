7e82f03073e51e29ac1bbed14f7a3a3c7aa5225c
Ethan Tang <itsethang5@gmail.com>
2024-12-23 19:36:22 -0500

    Implement jwt to Post Manage tabs

--- src/components/post/PostManage.tsx
+++ src/components/post/PostManage.tsx
@@ -1,26 +1,28 @@
 import { Button, Flex, Separator } from "@radix-ui/themes";
+import useIsAuthenticated from "react-auth-kit/hooks/useIsAuthenticated";
 import { Link } from "react-router-dom";
+import useAuthUserId from "../../hooks/useAuthUserId";
 import useGetUserById from "../../hooks/useGetUserById";
-import useUserStore from "../user/store";
 import usePostQueryStore from "./store";
 
 const PostManage = () => {
-  const { userId: loginId, isLoggedIn } = useUserStore();
+  const isAuth = useIsAuthenticated();
+  const userId = useAuthUserId();
   const { setUsername } = usePostQueryStore();
-  const { data: user } = isLoggedIn ? useGetUserById(loginId + "") : {};
+  const { data: user } = isAuth ? useGetUserById(userId) : {};
   return (
     <Flex align="center" gap="5">
       <Button variant="outline" onClick={() => setUsername("")}>
         🌎 Latest Posts
       </Button>
-      {isLoggedIn && user && (
+      {isAuth && user && (
         <>
           <Separator orientation="vertical" size="2" />
           <Button variant="outline" onClick={() => setUsername(user.username)}>
             My Posts
           </Button>
           <Separator orientation="vertical" size="2" />
-          <Link to={`/new/${loginId}`}>
+          <Link to={`/new/${userId}`}>
             <Button variant="outline" color="yellow">
               Make a Post 🚀
             </Button>
