c837cf4f7c88c543b7bc13117b87baacf88ba90d
Ethan Tang <itsethang5@gmail.com>
2024-12-24 13:40:49 -0500

    Add overflow to homegrid layout

--- src/pages/HomeGrid.tsx
+++ src/pages/HomeGrid.tsx
@@ -8,11 +8,15 @@ const HomeGrid = () => {
   const isAuth = useIsAuthenticated();
   return (
     <div className="grid grid-cols-5">
-      <div className={`col-span-5 ${isAuth && "lg:col-span-4"} m-4`}>
+      <div
+        className={`col-span-5 ${
+          isAuth && "lg:col-span-4"
+        } m-4 max-h-[700px] overflow-scroll`}
+      >
         <AllPost />
       </div>
       {isAuth && (
-        <div className="hidden col-span-1 lg:flex">
+        <div className="hidden col-span-1 lg:flex max-h-[700px] overflow-scroll">
           <FollowingList />
         </div>
       )}
