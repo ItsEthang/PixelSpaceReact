d5075340ba07da1b805ce9adcc1b8bb5cfc82b5b
Ethan Tang <itsethang5@gmail.com>
2024-12-23 19:07:00 -0500

    implement jwt for follower page

--- src/pages/user/UserFollower.tsx
+++ src/pages/user/UserFollower.tsx
@@ -1,27 +1,15 @@
-import { useParams } from "react-router-dom";
 import { Box, Card, Flex, Heading, Separator } from "@radix-ui/themes";
+import { useParams } from "react-router-dom";
 import PostUser from "../../components/post/PostUser";
+import useAuthUserId from "../../hooks/useAuthUserId";
 import useGetFollower from "../../hooks/useGetFollower";
-import useUserStore from "../../components/user/store";
 import useGetUserById from "../../hooks/useGetUserById";
 
 const UserFollowers = () => {
   const params = useParams();
-  const { isLoggedIn, userId } = useUserStore();
   const { data: followers } = useGetFollower(+params.userId!);
   const { data: profile } = useGetUserById(params.userId!);
-  if (!isLoggedIn) {
-    return (
-      <Flex justify="center" m="5">
-        <Box>
-          <Heading as="h2" size="8" mb="3">
-            Please log in to see your followers
-          </Heading>
-          <Separator size="4" />
-        </Box>
-      </Flex>
-    );
-  }
+  const userId = useAuthUserId();
   return (
     <>
       <Flex justify="center" m="5">
