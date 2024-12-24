fd99518eab0370b66613402fcae6612ebd4d263e
Ethan Tang <itsethang5@gmail.com>
2024-12-23 23:38:46 -0500

    Responsive: Post content title

--- src/components/post/PostContent.tsx
+++ src/components/post/PostContent.tsx
@@ -5,7 +5,7 @@ import ReactMarkdown from "react-markdown";
 const PostContent = ({ post }: { post: Post }) => {
   return (
     <Box my="3">
-      <Text as="div" size="6" weight="bold" mb="2">
+      <Text as="div" size="6" weight="bold" mb="2" className="line-clamp-1">
         {post.title}
       </Text>
       <Text as="div" size="2" color="gray" className="line-clamp-3">
